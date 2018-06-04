dee18ec2367f0f3bf7ba56d96e92045ee46acbc2
jewell86 <jewellsmailbox@gmail.com>
2018-06-04 11:29:49 -0700

    take 4 photos on timer & disp on print page w/ print button

--- bundle.js
+++ bundle.js
@@ -1,21 +1,39 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// let takePhotos = require(./takePhotos )
-// const takePrequire('./takePhotos')
 
 (function() {
-  var width = 320;
-  var height = 0;
-  var streaming = false;
-  var video = null;
-  var canvas = null;
-  var photo1 = document.querySelector("#photo1");
-  var startbutton = null;
+  let width = 320;
+  let height = 0;
+  let streaming = false;
+  let video = null;
+  let canvas1 = document.querySelector('#canvas1')
+  let canvas2 = document.querySelector('#canvas2')
+  let canvas3 = document.querySelector('#canvas3')
+  let canvas4 = document.querySelector('#canvas4')
+  let photo1 = document.querySelector("#photo1");
+  let photo2 = document.querySelector("#photo2");
+  let photo3 = document.querySelector("#photo3");
+  let photo4 = document.querySelector("#photo4");
+  let image1 = null
+  let image2 = null
+  let image3 = null
+  let image4 = null
+  let data1 = null
+  let data2 = null
+  let data3 = null
+  let data4 = null
+  let photo1string = null
+  let photo2string = null
+  let photo3string = null
+  let photo4string = null
+
+  var startButton = null;
+
 
   function startup() {
     video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
+    // canvas = document.getElementById('canvas');
+    // photo = document.getElementById('photo');
+    startButton = document.getElementById('startbutton');
 
     navigator.getMedia = ( navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
@@ -44,111 +62,93 @@
     video.addEventListener('canplay', function(ev){
       if (!streaming) {
         height = video.videoHeight / (video.videoWidth/width);
-
-        // Firefox currently has a bug where the height can't be read from
-        // the video, so we will make assumptions if this happens.
-
         if (isNaN(height)) {
           height = width / (4/3);
         }
-
+//canvas?
         video.setAttribute('width', width);
         video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
+        // canvas.setAttribute('width', width);
+        // canvas.setAttribute('height', height);
         streaming = true;
       }
     }, false);
 
-    startbutton.addEventListener('click', function(ev){
-      takepicture();
+
+
+    let index = 0
+
+
+
+    let params = [[ canvas1, data1, 'image1', photo1, 'str1', photo1string ],
+    [ canvas2, data2, 'image2', photo2, 'str2', photo2string ],
+    [ canvas3, data3, 'image3', photo3, 'str3', photo3string ],
+    [ canvas4, data4, 'image4', photo4, 'str4', photo4string ]]
+
+let parameter = params[index]
+
+
+
+  startButton.addEventListener('click', function(ev){
+      window.setTimeout(function() { takepicture(...parameter) }, 1000)
       ev.preventDefault();
-    }, false);
+  }, false);
 
-    clearphoto();
-  }
 
-  // Fill the photo with an indication that none has been
-  // captured.
+  function takepicture( can, dat, img, pho, strName, str) {
+    console.log("takepic");
+    let context = can.getContext('2d');
 
-  function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
-
-  function takepicture() {
-    var context = canvas1.getContext('2d');
     if (width && height) {
-      canvas1.width = width;
-      canvas1.height = height;
-      context.drawImage(video, 0, 0, width, height);
-      var data = canvas1.toDataURL('image/png');
-      photo1.setAttribute('src', data);
-      canvas1.classList.add('d-none')
-      setTimeout(takepicture2, 1000);
-    } else {
-      clearphoto();
-    }
-  }
+    can.width = width;
+    can.height = height;
+    context.drawImage(video, 0, 0, width, height);
+    dat = can.toDataURL(`${img}/png`);
+    pho.setAttribute('src', dat);
+    can.classList.add('d-none')
+    str = dat.replace(/^data:image\/(png|jpg);base64,/, "")
+    localStorage.setItem(strName, str)
+    parameter = params[index]
 
-  function takepicture2() {
-    var context = canvas2.getContext('2d');
-    if (width && height) {
-      canvas2.width = width;
-      canvas2.height = height;
-      context.drawImage(video, 0, 0, width, height);
-      var data2 = canvas2.toDataURL('image2/png');
-      photo2.setAttribute('src', data2);
-      canvas2.classList.add('d-none')
-      setTimeout(takepicture3, 1000);
-    } else {
-      clearphoto();
-    }
+    if( index < 4){
+    index++
+    console.log(parameter)
+    window.setTimeout(function () { takepicture(...parameter) }, 1000)
   }
-
-  function takepicture3() {
-    var context = canvas3.getContext('2d');
-    if (width && height) {
-      canvas3.width = width;
-      canvas3.height = height;
-      context.drawImage(video, 0, 0, width, height);
-      var data3 = canvas3.toDataURL('image3/png');
-      photo3.setAttribute('src', data3);
-      canvas3.classList.add('d-none')
-      setTimeout(takepicture4, 1000);
-    } else {
-      clearphoto();
-    }
   }
+}
 
-  function takepicture4() {
-    var context = canvas4.getContext('2d');
-    if (width && height) {
-      canvas4.width = width;
-      canvas4.height = height;
-      context.drawImage(video, 0, 0, width, height);
-      var data4 = canvas4.toDataURL('image4/png');
-      photo4.setAttribute('src', data4);
-      canvas4.classList.add('d-none')
-    } else {
-      clearphoto();
-    }
-  }
 
-  // Set up our event listener to run the startup process
-  // once loading is complete.
+}
+
   window.addEventListener('load', startup, false);
-})();
+
+
+
+let hiddenEmail = document.querySelector('.hidden-email')
+let emailButton = document.querySelector('.email-button')
+let hiddenSubmit = document.querySelector('.hidden-submit')
+
+
+emailButton.addEventListener("click", function(){
+  hiddenEmail.classList.remove('d-none')
+  hiddenSubmit.classList.remove('d-none')
+
+})})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = main
 
