b79b86fe026e0a6afa6476269647ae34d33dc150
jewell86 <jewellsmailbox@gmail.com>
2018-06-06 10:05:44 -0700

    add photostrip & text to print page

--- bundle.js
+++ bundle.js
@@ -1,33 +1,22 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-
-(function() {
-  let width = 420
-  let height = 400
-  let streaming = false
-  let video = null
-  let startButton = null
-  let paragraph = document.querySelector('.photo-title')
+  window.addEventListener('load', startup, false)
   let titleSubmit = document.querySelector('.title-submit')
-  let titleValue = document.querySelector('.title-value')
   let printButton = document.querySelector('.print-button')
   let filters = Array.from(document.querySelectorAll('.filter-button'))
-  let photos = Array.from(document.querySelectorAll('.photos'))
-  let newFilter =
-
-  filters.forEach(function(filter) {
-    filter.addEventListener('click', function(ev){
-      newFilter = filter.dataset.filter
-      photos.forEach(function(photo) {
-      photo.removeAttribute("class")
-      photo.classList.add(newFilter)
-      })
-    })
-  })
+  let newFilter
+  let newTitleValue
+  let startButton = document.querySelector('#startbutton')
 
   function startup() {
+    localStorage.clear()
+    if(!!window.performance && window.performance.navigation.type == 2) {
+      window.location.reload();
+    }
+    let width = 420
+    let height = 400
+    let streaming = false
     video = document.getElementById('video')
     startButton = document.getElementById('startbutton');
-
     navigator.getMedia = ( navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
                            navigator.mozGetUserMedia ||
@@ -52,7 +41,6 @@
         console.log("An error occured! " + err)
       }
     )
-
     video.addEventListener('canplay', function(ev){
       if (!streaming) {
         height = video.videoHeight / (video.videoWidth/width)
@@ -66,25 +54,36 @@
     }, false)
 
     titleSubmit.addEventListener('submit', function(event) {
+      let paragraph = document.querySelector('.photo-title')
+      let titleValue = document.querySelector('.title-value')
       paragraph.innerText = titleValue.value
+      // window.locaStorage.setItem('titleValue', titleValue )
       event.preventDefault()
+      newTitleValue = titleValue.value
+
     })
 
-    // let hiddenEmail = document.querySelector('.hidden-email')
-    // let emailButton = document.querySelector('.email-button')
-    // let hiddenSubmit = document.querySelector('.hidden-submit')
-    //
-    //   emailButton.addEventListener("click", function(){
-    //   hiddenEmail.classList.remove('d-none')
-    //   hiddenSubmit.classList.remove('d-none')
-    //
-    // })
-    //
-    let filter = JSON.stringify(newFilter);
+    startButton.addEventListener('click', function(ev){
+        window.setTimeout(function() { takePicture(currentPhoto) }, 1000)
+        ev.preventDefault();
+    }, false);
+
+    filters.forEach(function(filter) {
+      filter.addEventListener('click', function(ev){
+        let photos = Array.from(document.querySelectorAll('.photos'))
+        newFilter = filter.dataset.filter
+        photos.forEach(function(photo) {
+        photo.removeAttribute("class")
+        photo.classList.add(newFilter)
+        })
+      })
+    })
 
     printButton.addEventListener('click', function(event) {
-      window.localStorage.setItem('filter', newFilter);
+    window.localStorage.setItem('filter', newFilter)
+    window.localStorage.setItem('newTitleValue', newTitleValue)
     })
+    }
 
     let photoData = [{ canvas : document.querySelector("#canvas1"), photo : document.querySelector("#photo1") },
       { canvas : document.querySelector("#canvas2"), photo : document.querySelector("#photo2") },
@@ -92,11 +91,6 @@
       { canvas : document.querySelector("#canvas4"), photo : document.querySelector("#photo4") }]
     let index = 0
     let currentPhoto = photoData[index]
-
-    startButton.addEventListener('click', function(ev){
-        window.setTimeout(function() { takePicture(currentPhoto) }, 1000)
-        ev.preventDefault();
-    }, false);
 
     function takePicture(currentPhoto) {
       currentPhoto.photo.classList.remove("d-none")
@@ -112,18 +106,5 @@
         window.setTimeout(function () { takePicture(currentPhoto) }, 1000)
       }
     }
-}
-  window.addEventListener('load', startup, false)
-
-})()
-
-
-
-
-
-
-
-
-// module.exports = main
 
 },{}]},{},[1]);
